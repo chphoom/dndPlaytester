83add0e805b513e39c2d57b0c11e68b018c3ba32
Chalisa Keaw Phoomsakha <44123218+chphoom@users.noreply.github.com>
2025-02-04 21:40:51 -0500

    Create main.ts
    warm ups

---
+++ main.ts
@@ -0,0 +1,5 @@
+// alright let's gt started
+// this is a simple program for self fulfillment purposes, and wil basically be the programming version of 
+// incoherent rambling
+
+console.log("Hello World");
